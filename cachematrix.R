570bb8f9c6da2d73d5733ed040cc99536f00a635
tamaltaru <debmalya.sinha@gmail.com>
2015-06-21 15:34:40 -0700

    Completion of assignment2
    stable build, not working for singular matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the fonction takes a matrix and keps its matrix in cache, if called repeatedly, 
+## it tells that it's getting the data from cache without repeatative calculation
 
-## Write a short comment describing this function
+## This function makes the inverse and stores it in the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makecacheMatrix <- function(x= matrix()) {
+  inmat <- NULL
+  set <- function(y) {
+    x <<- y
+    inmat <<- NULL
+  }
+  get <- function() x
+  setmat <- function(solve) inmat <<- solve
+  getmat <- function() inmat
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
 }
 
+##########################################################
+# this function compares the exsiting condition andecides wheather it need to calculate again 
+#or it can pass the matrix stored.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  inmat <- x$getmat()
+  if(!is.null(inmat)) {
+    message("getting cached data")
+    return(inmat)
+  }
+  data <- x$get()
+  inmat <- solve(data, ...)
+  x$setmat(inmat)
+  inmat
 }
+
+##########################################################
+
+# example
+a<- matrix(rnorm(16, 5,4), nrow=4)
+a
+b<- makecacheMatrix(a)
+b
+inv <-cachesolve(b)
+l <- a %*% inv
+l
